c5f7fc44a26991a106f59a3c311abe7627951c30
Juan Martin Varela <varelajumartin@gmail.com>
2018-11-03 17:14:17 -0300

    se modifica handlebars

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 'use strict'
-let templateTareas;
+let templateComentarios;
 
-fetch('js/templates/tareas.handlebars')
+fetch('js/templates/comentarios.handlebars')
 .then(response => response.text())
 .then(template => {
-    templateTareas = Handlebars.compile(template); // compila y prepara el template
+    templateComentarios = Handlebars.compile(template); // compila y prepara el template
 
-    getTareas();
+    getComentarios();
 });
 
 
-function getTareas() {
-    fetch("api/tarea")
+function getComentarios() {
+    let obj = document.querySelector('.contenedor_comentarios');
+    console.log(obj.dataset);
+    /*fetch("api/comentarios/temporada/:ID1/episodio/:ID2")
     .then(response => response.json())
-    .then(jsonTareas => {
-        mostrarTareas(jsonTareas);
-    })
+    .then(jsonComentarios => {
+        mostrarComentarios(jsonComentarios);
+    })*/
 }
 
-function mostrarTareas(jsonTareas) {
+function mostrarComentarios(jsonComentarios) {
     let context = { // como el assign de smarty
-        tareas: jsonTareas,
+        comentarios: jsonComentarios,
         otra: "hola"
     }
-    let html = templateTareas(context);
-    document.querySelector("#tareas-container").innerHTML = html;
+    let html = templateComentarios(context);
+    document.querySelector(".contenedor_comentarios").innerHTML = html;
 }
